d06fc6cc7da09ef5393a15f024a87bba7f575a6e
kiran <gandikotakiran99@gmail.com>
2022-01-10 17:50:19 +0530

    start of react-router

---
+++ src/index.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+
+import {BrowserRouter as Router,Routes,Route,Navigate,Link ,Outlet} from 'react-router-dom'
+
+ReactDOM.render(
+ <Router>
+   <Routes>
+     <Route path="/" element={<Home/>}/>
+     <Route path="/myapps" element={<Navigate replace to="/learn" /> } />
+     <Route path="/learn" element={<Learn/>}>
+       <Route path="courses" element={<Course/>} />
+       <Route path="bundles" element={<Bundle/>} />
+     </Route>
+   </Routes>
+ </Router>,
+  document.getElementById('root')
+);
+
+function Home(){
+  return(
+    <div>
+      <h1>Home route added</h1>
+    </div>
+  )
+}
+
+function Learn(){
+  return(
+    <div>
+      Learn
+      <h4>All Courses are listed Here</h4>
+      <Link className='btn btn-success' to="/learn/courses">
+        Course
+      </Link>
+         {" "}
+      <Link className='btn btn-primary' to="/learn/bundles">
+        Bundle
+      </Link>
+
+      <Outlet />
+
+    </div>
+  )
+}
+
+function Course(){
+  return(
+    <div>
+      Course List
+      <h4>Course Card</h4>
+    </div>
+  )
+}
+
+function Bundle(){
+  return(
+    <div>
+      Bundle List
+      <h4>Bundle Card</h4>
+    </div>
+  )
+}
+
+
+reportWebVitals();
